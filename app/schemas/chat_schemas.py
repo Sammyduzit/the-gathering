8fb54dfbfd8e76ddf12110bad00f82d3740a23e8
Sammyduzit <sammy.duzit999@gmail.com>
2025-06-20 18:38:43 +0200

    Add Conversation schemas

--- app/schemas/chat_schemas.py
+++ app/schemas/chat_schemas.py
@@ -11,14 +11,38 @@ class MessageCreate(BaseModel):
 
 class MessageResponse(BaseModel):
     """
-    Basic message response.
+    Message response.
     """
     id : int
     sender_id: int
     sender_username: str
     content: str
     sent_at: datetime
-    room_id: int
+
+    room_id: int | None = Field(None, description="Room ID for room-wide chat")
+    conversation_id: int | None =  Field(None, description="Conversation ID for private/group chat")
 
     model_config = ConfigDict(from_attributes=True)
 
+
+class ConversationCreate(BaseModel):
+    """
+    Schema for creating conversations.
+    """
+    participant_usernames: list[str] = Field(min_length=1, max_length=20,
+                                             description="List of usernames to include in conversation")
+    conversation_type: str = Field(description="'private' (2 users) or 'group' (2+ users)")
+
+
+
+class ConversationResponse(BaseModel):
+    """
+    Conversation response.
+    """
+    id: int
+    conversation_type: str
+    room_id: int
+    is_active: bool
+    created_at: datetime
+
+    model_config = ConfigDict()
